df35db5d224286ad6d5bb995106a538ec2ca93c1
Cheewai <cheewai.lai+github@gmail.com>
2016-05-17 12:46:37 +0200

    Update docker-entrypoint.sh
    Added exec

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,4 +5,4 @@ set -eux
 adduser -s /bin/false -D -h $CATALINA_HOME -H -u ${TOMCAT_UID} tomcat \
  && chown -R tomcat $CATALINA_HOME/* \
  && chmod +x $CATALINA_HOME/bin/setenv.sh
-gosu tomcat catalina.sh run
+exec gosu tomcat catalina.sh run
