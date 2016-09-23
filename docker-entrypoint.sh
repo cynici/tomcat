56437fa4a3ca92fb0690a6ce9c922a771ac77a09
Cheewai Lai <clai@csir.co.za>
2016-09-23 14:06:58 +0200

    Run commands separately in docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,7 @@
 
 TOMCAT_UID="${TOMCAT_UID:-1000}"
 set -eux
-adduser -s /bin/false -D -h $CATALINA_HOME -H -u ${TOMCAT_UID} tomcat \
- || true \
- && chown -R tomcat $CATALINA_HOME/* \
- && chmod +x $CATALINA_HOME/bin/setenv.sh
+adduser -s /bin/false -D -h $CATALINA_HOME -H -u ${TOMCAT_UID} tomcat
+chown -R tomcat $CATALINA_HOME/*
+chmod +x $CATALINA_HOME/bin/setenv.sh
 exec gosu tomcat $CATALINA_HOME/bin/catalina.sh run
