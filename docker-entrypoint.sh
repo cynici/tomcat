aafff3b1d2de63ecba44c26360021300945ff8fc
Cheewai Lai <clai@clai.csir.co.za>
2016-10-26 09:28:13 +0200

    Added dockerize hook in docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,4 +5,5 @@ set -ux
 adduser -s /bin/false -D -h $CATALINA_HOME -H -u ${TOMCAT_UID} tomcat
 chown -R tomcat $CATALINA_HOME/*
 chmod +x $CATALINA_HOME/bin/setenv.sh
+test -n "$DOCKERIZE_CMD" && gosu tomcat $DOCKERIZE_CMD
 exec gosu tomcat $CATALINA_HOME/bin/catalina.sh run
