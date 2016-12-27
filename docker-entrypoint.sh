c8102bc0c33e5ad9ccb75665e09375abf09d1bac
Cheewai Lai <clai@csir.co.za>
2016-12-27 15:23:30 +0200

    DOCKERIZE_CMD in docker-entrypoint.sh need a default blank value

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,12 @@
 #! /bin/sh
 
 TOMCAT_UID="${TOMCAT_UID:-1000}"
+DOCKERIZE_CMD="{DOCKERIZE_CMD:-}"
 set -ux
 adduser -s /bin/false -D -h $CATALINA_HOME -H -u ${TOMCAT_UID} tomcat
 chown -R tomcat $CATALINA_HOME/*
 chmod +x $CATALINA_HOME/bin/setenv.sh
-if [ -n "$DOCKERIZE_CMD:-" ]; then
+if [ -n "$DOCKERIZE_CMD" ]; then
   gosu tomcat $DOCKERIZE_CMD || exit $?
 fi
 exec gosu tomcat $CATALINA_HOME/bin/catalina.sh run
