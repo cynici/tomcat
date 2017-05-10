7b91aef48fdf1216de5ed68260448a52bd0f067d
Cheewai Lai <clai@csir.co.za>
2017-05-10 11:22:55 +0200

    Updated to tomcat-8.5.14, dockerize-0.4.0

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 TOMCAT_UID="${TOMCAT_UID:-1000}"
 DOCKERIZE_CMD="${DOCKERIZE_CMD:-}"
-set -ux
+set -u
 adduser -s /bin/false -D -h $CATALINA_HOME -H -u ${TOMCAT_UID} tomcat
 chown -R tomcat $CATALINA_HOME/*
 chmod +x $CATALINA_HOME/bin/setenv.sh
