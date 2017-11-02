0608257f3d3689c1f0ffa2bb303e02aaae60f06f
Cheewai Lai <clai@csir.co.za>
2017-11-02 14:17:23 +0200

    Inject DOCKERIZE_CMD if it is defined

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,9 @@
 #! /bin/sh
-
 TOMCAT_UID="${TOMCAT_UID:-1000}"
 DOCKERIZE_CMD="${DOCKERIZE_CMD:-}"
 set -u
+# Refer to busybox 'adduser' manpage for details
 adduser -s /bin/false -D -h $CATALINA_HOME -H -u ${TOMCAT_UID} tomcat
 chown -R tomcat $CATALINA_HOME/*
 chmod +x $CATALINA_HOME/bin/setenv.sh
-if [ -n "$DOCKERIZE_CMD" ]; then
-  gosu tomcat $DOCKERIZE_CMD || exit $?
-fi
-exec gosu tomcat $CATALINA_HOME/bin/catalina.sh run
+exec gosu tomcat $DOCKERIZE_CMD $CATALINA_HOME/bin/catalina.sh run
